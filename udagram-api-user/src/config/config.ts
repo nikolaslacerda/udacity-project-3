4f39647068c4b6d337be325b56ddbc75f07398cb
Nikolas <nikolas.lacerda@hotmail.com>
2023-03-03 02:24:37 -0300

    add project

---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': "root",
+  'password': "adminadmin",
+  'database': "postgres",
+  'host': "database-project-3.cf2ayvjbtx6y.us-east-1.rds.amazonaws.com",
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'project-4',
+  'aws_media_bucket': 'nikolas-user-image-uploads',
+  'url': process.env.URL,
+  'jwt': {
+    'secret': 'testing',
+  },
+};
